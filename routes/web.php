f0a0cb4ce480759f4b122c82979a679f1a689308
ersyvh <erlanggafs05@gmail.com>
2024-02-13 20:22:30 +0700

    init first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard');
+    })->name('home');
+    Route::resource('users', UserController::class);
+
+    // Route::get('/users', function () {
+    //     return view('pages.users');
+    //     })->name('users');
+
+    // Route::get('/profile', function () {
+    //     return view('pages.profile');
+    //     })->name('profile');
+});
