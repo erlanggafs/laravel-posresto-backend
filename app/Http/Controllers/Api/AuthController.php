837cc9cba15705da0531890a5838fa2356b77329
ersyvh <erlanggafs05@gmail.com>
2024-02-16 21:25:36 +0700

    create api login-product-categories

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,46 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Termwind\Components\Raw;
 
 class AuthController extends Controller
 {
-    //
+    //login api
+    public function login(Request $request)
+    {
+        //validate the request
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        //check if the user exist
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        //check if the password is correct
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        //generate token
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'success',
+            'token' => $token,
+            'user' => $user,
+        ], 200);
+    }
 }
